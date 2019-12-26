b106088f0327370c0d49ff25505e72b057fea5fe
Aliaksei Milto <alexeymilto@gmail.com>
2019-12-26 20:33:35 +0300

    Added getting books by name,author and description to db

--- main.go
+++ main.go
@@ -95,7 +95,7 @@ func ServeContent(w http.ResponseWriter, r *http.Request) {
 	t := time.Now()
 
 	if pageAlias == "" {
-		pageAlias = "home"
+		pageAlias = "index.html"
 	}
 
 	context := defaultContext{}
@@ -115,12 +115,27 @@ func ServeContent(w http.ResponseWriter, r *http.Request) {
 	staticPage.Execute(w, context)
 }
 
+func updateHTML(w http.ResponseWriter, r *http.Request) {
+	urlParams := mux.Vars(r)
+	pageAlias := urlParams["pageAlias"]
+
+	staticPages = PopulateStaticPages()
+	staticPage := staticPages.Lookup(pageAlias)
+	if staticPage == nil {
+		staticPage = staticPages.Lookup("404.html")
+		w.WriteHeader(404)
+	}
+
+	staticPage.Execute(w, nil)
+}
+
 // WebServer - Server Function w/ Routers
 func WebServer() {
 	route := mux.NewRouter()
 
 	route.HandleFunc("/", ServeContent)
-	route.HandleFunc("/{pageAlias}", ServeContent) // Dynamic URL
+	route.HandleFunc("/{pageAlias}", ServeContent)          // Dynamic URL
+	route.HandleFunc("/updateHTML/{pageAlias}", updateHTML) //For updating new html files online
 
 	http.HandleFunc("/css/", ServeResource)
 	http.HandleFunc("/js/", ServeResource)
@@ -131,7 +146,8 @@ func WebServer() {
 	route.HandleFunc("/create/{pageAlias}", wannaCreateBook)
 	route.HandleFunc("/create/books/{pageAlias}", createBook).Methods("POST")
 	route.HandleFunc("/books/{pageAlias}", getAllBooks).Methods("GET")
-	route.HandleFunc("/books/{Name}/{pageAlias}", getBookByName).Methods("GET")
+	route.HandleFunc("/{Name}/book-name.html", getBookByName).Methods("GET")
+	route.HandleFunc("/{Author}/book-author.html", getBookByAuthor).Methods("GET")
 
 	http.Handle("/", route)
 
@@ -152,10 +168,11 @@ const (
 )
 
 type Book struct {
-	Name   string
-	Author string
-	Price  float32
-	ID     int
+	Name        string
+	Author      string
+	Price       float32
+	Description string
+	ID          int
 }
 
 func Open() (*sql.DB, error) {
@@ -170,35 +187,6 @@ func Open() (*sql.DB, error) {
 		log.Fatal(err)
 	}
 	return db, nil
-}
-
-func getBookByName(w http.ResponseWriter, r *http.Request) {
-	urlParams := mux.Vars(r)
-	Name := urlParams["Name"]
-	pageAlias := urlParams["pageAlias"]
-
-	Book := getBookByNameFromDb(Name)
-
-	staticPage := staticPages.Lookup(pageAlias)
-
-	if staticPage == nil {
-		staticPage = staticPages.Lookup("404.html")
-		w.WriteHeader(404)
-	}
-
-	staticPage.Execute(w, Book)
-}
-
-func getBookByNameFromDb(name string) Book {
-	dbase, err := Open()
-	if err != nil {
-		log.Fatal(err)
-	}
-	row := dbase.QueryRow("select * from books where name=$1", name)
-	b := Book{}
-	err := row.Scan(&b.Name, &b.Author, &b.Price, &b.ID)
-	fmt.Println("Scan success")
-	return b
 }
 
 func GetPopularBooks(w http.ResponseWriter, r *http.Request) {
@@ -226,14 +214,14 @@ func getPopularBooksFromDb() []Book {
 
 	popularBooks := []Book{}
 
-	rows, err := dbase.Query("select * from \"books\" limit 3")
+	rows, err := dbase.Query("select name, author, price from \"books\" limit 3")
 	if err != nil {
 		panic(err)
 	}
 
 	t := Book{}
 	for rows.Next() {
-		err := rows.Scan(&t.Name, &t.Author, &t.Price, &t.ID)
+		err := rows.Scan(&t.Name, &t.Author, &t.Price)
 		if err != nil {
 			fmt.Println(err)
 			continue
@@ -263,13 +251,13 @@ func getAllBooksFromDb() []Book {
 		log.Fatal(err)
 	}
 	Books := []Book{}
-	rows, err := dbase.Query("select * from books")
+	rows, err := dbase.Query("select name, author, price from books")
 	if err != nil {
 		log.Fatal(err)
 	}
 	b := Book{}
 	for rows.Next() {
-		err := rows.Scan(&b.Name, &b.Author, &b.Price, &b.ID)
+		err := rows.Scan(&b.Name, &b.Author, &b.Price)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -295,9 +283,10 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 	pageAlias := urlParams["pageAlias"]
 	price, _ := strconv.ParseFloat(r.FormValue("Price"), 32)
 	book := Book{
-		Name:   r.FormValue("Name"),
-		Author: r.FormValue("Author"),
-		Price:  float32(price),
+		Name:        r.FormValue("Name"),
+		Author:      r.FormValue("Author"),
+		Price:       float32(price),
+		Description: r.FormValue("Description"),
 	}
 	createdBook := createBookInDb(book)
 	staticPage := staticPages.Lookup(pageAlias)
@@ -309,13 +298,13 @@ func createBookInDb(book Book) Book {
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = dbase.Exec("insert into books (name, author, price) values($1, $2, $3);", book.Name, book.Author, book.Price)
+	_, err = dbase.Exec("insert into books (name, author, price, description) values($1, $2, $3, $4);", book.Name, book.Author, book.Price, book.Description)
 	if err != nil {
 		log.Fatal(err)
 	}
-	row := dbase.QueryRow("select * from books where id=(select max(id) from books)")
+	row := dbase.QueryRow("select name, author, price from books where id=(select max(id) from books)")
 	b := Book{}
-	err = row.Scan(&b.Name, &b.Author, &b.Price, &b.ID)
+	err = row.Scan(&b.Name, &b.Author, &b.Price)
 	fmt.Println("Scan success")
 	if err != nil {
 		fmt.Println(err)
@@ -323,14 +312,58 @@ func createBookInDb(book Book) Book {
 	return b
 }
 
-/*func createBook(w http.ResponseWriter, r *http.Request) {
+func getBookByName(w http.ResponseWriter, r *http.Request) {
 	urlParams := mux.Vars(r)
-	pageAlias := urlParams["pageAlias"]
-	json.NewDecoder(r.Body).Decode(&newTask)
-	newBook := createBookInDb()
+	Name := urlParams["Name"]
 
-	task := createBookInDb(newTask)
-	json.NewEncoder(w).Encode(task)
+	Book := getBookByNameFromDb(Name)
+
+	staticPage := staticPages.Lookup("book-name.html")
+
+	if staticPage == nil {
+		staticPage = staticPages.Lookup("404.html")
+		w.WriteHeader(404)
+	}
+
+	staticPage.Execute(w, Book)
 }
 
-*/
+func getBookByNameFromDb(name string) Book {
+	dbase, err := Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	row := dbase.QueryRow("select * from books where name=$1", name)
+	b := Book{}
+	err = row.Scan(&b.Name, &b.Author, &b.Price, &b.Description, &b.ID)
+	fmt.Println("Scan success")
+	return b
+}
+
+func getBookByAuthor(w http.ResponseWriter, r *http.Request) {
+	urlParams := mux.Vars(r)
+	Name := urlParams["Author"]
+
+	Book := getBookByAuthorFromDb(Name)
+
+	staticPage := staticPages.Lookup("book-author.html")
+
+	if staticPage == nil {
+		staticPage = staticPages.Lookup("404.html")
+		w.WriteHeader(404)
+	}
+
+	staticPage.Execute(w, Book)
+}
+
+func getBookByAuthorFromDb(author string) Book {
+	dbase, err := Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	row := dbase.QueryRow("select * from books where author=$1 ", author)
+	b := Book{}
+	err = row.Scan(&b.Name, &b.Author, &b.Price, &b.Description, &b.ID)
+	fmt.Println("Scan success")
+	return b
+}
